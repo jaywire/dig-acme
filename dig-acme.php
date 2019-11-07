2dbd5d660d488a3fcf046a003fba0a008d20f927
jaywire <jaywire@protonmail.com>
2019-11-07 11:16:18 -0500

    added $dnsIP function and comments
    Added $dnsIP so user can provide their own DNS server IP for the check.
    
    Added logic to use a script default is user does not provide valid IPv4 address.
    
    Provided better comments

--- dig-acme.php
+++ dig-acme.php
@@ -1,9 +1,11 @@
 #!/usr/bin/php
 <?php
 
+// defininig variables
 $cmdDig = '/usr/bin/dig';
 $acme = '_acme-challenge.';
 $dmgdev = '._acme-dns.dmgdev.com.';
+$dnsDefault = '1.1.1.1';
 
 // checking that dig exists on machine
 if (file_exists($cmdDig)) {
@@ -12,7 +14,7 @@ if (file_exists($cmdDig)) {
     echo "\n*** ERROR *** ||| $cmdDig not found, please install dig\n" . PHP_EOL;
     exit();
 }
-
+// validate $domain provided by user is a valid domain
 function validDom($domain) {
     if(!preg_match("/^(?!\-)(?:[a-zA-Z\d\-]{0,62}[a-zA-Z\d]\.){1,126}(?!\d+)[a-zA-Z\d]{1,63}$/i", $domain) ) {
     return false;
@@ -22,16 +24,39 @@ return $domain;
 
 echo "What domain are we validating? ";
 
-// take input if domain fails regex check and loop back through
+// Give error and take input again if $domain fails regex check and loop back through validDom function
 while(validDom($domain) == false) {
     $domain = rtrim(fgets(STDIN));
     if(validDom($domain) == false) {
-        echo "Please enter a valid domain. Do not include the protocol (http, https): ";
+        echo "***ERROR!* Please enter a valid domain. Do not include the protocol (http, https): ";
     }
 }
+
+// validate $dnsIP provided by user is a valid IPV4 address
+function validIP($dnsIP) {
+    if(!preg_match("/^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$/i", $dnsIP) ) {
+    return false;
+    }
+return $dnsIP;
+}//end validIP function.
+
+echo "What DNS server are we using? To use the default, hit Enter: ";
+
+// Give error and take input again if $dnsIP fails regex check and loop back through validIP function
+// If user does not want to provide their own DNS server, use $dnsDefault
+while(validIP($dnsIP) == false) {
+    $dnsIP = rtrim(fgets(STDIN));
+    if ($dnsIP == null) {
+        echo "\nOK - using default DNS (".$dnsDefault.") for this check.\n";
+        $dnsIP = $dnsDefault;
+    } elseif(validIP($dnsIP) == false) {
+        echo "***ERROR!*** Please enter a valid IPV4 address. If you'd like to use the default, hit ENTER: ";
+    }
+}
+
 // running dig command if dig exists and domain passes validation
 if($digCheck) {
-    $acmeCheck = "$cmdDig @1.1.1.1 ".$acme.$domain." cname +short";
+    $acmeCheck = "$cmdDig @".$dnsIP." ".$acme.$domain." cname +short";
     $strLookup = `$acmeCheck`;
     $strLookup = trim($strLookup);
 } 
@@ -39,7 +64,7 @@ if($digCheck) {
 if ($strLookup == $domain.$dmgdev) {
     echo "\n\nSUCCESS! ".$domain." HAS A VALID ACME-CHALLENGE RECORD!\n\nRETURNED HOST: ".$acme.$domain."\nEXPECTED HOST: ".$acme.$domain."\n\nRETURNED TARGET: ".$strLookup."\nEXPECTED TARGET: ".$domain.$dmgdev."\n\n".$acme.$domain." is an alias for ".$domain.$dmgdev."\n\n";
 } elseif ($strLookup != $domain.$dmgdev) {
-    $acmeHostCheck = "$cmdDig @1.1.1.1 ".$acme.$domain.".".$domain." cname +short";
+    $acmeHostCheck = "$cmdDig @".$dnsIP." ".$acme.$domain.".".$domain." cname +short";
     $strLookupFail = `$acmeHostCheck`;
     $strLookupFail = trim($strLookupFail);
     if ($strLookupFail == $domain.$dmgdev) {
